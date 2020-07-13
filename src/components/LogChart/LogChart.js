ca1edce85a5d8988643034e0f54833f2d064495b
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2020-07-13 11:41:33 -0400

    support start and end datepicker controls

--- src/components/LogChart/LogChart.js
+++ src/components/LogChart/LogChart.js
@@ -1,8 +1,30 @@
 import React from 'react';
 // import { FormattedMessage } from 'react-intl';
 import Chart from 'chart.js'
+import moment from 'moment'
 
 var blue = 'rgb(54, 162, 235)'
+
+function listToDatasets(resp) {
+  let groups = {}, buckets = []
+  for(let i=0; i<resp.length; i++) {
+    const item = resp[i]
+    if(item['tableName'] === '') item['tableName'] = '[no table]'
+    if(!(item['tableName'] in groups)) {
+      const idx = buckets.push({
+        "label" : item['tableName'],
+        "data": []
+      })
+      groups[item['tableName']] = idx - 1
+    }
+    const idx = groups[item['tableName']]
+    buckets[idx].data.push({
+      x: new Date(item["logTime"]),
+      y: item["elapsedTime"]
+    })
+  }
+  return buckets
+}
 
 // Must be a class to hold a ref
 class LogChart extends React.Component {
@@ -10,43 +32,57 @@ class LogChart extends React.Component {
     super(props);
     this.chartRef = React.createRef();
   }
-  componentDidUpdate(prevProps) {
-    if(prevProps != this.props && this.props && this.props.series && this.props.series.length > 0) {
-      const { series } = this.props
-      const myChartRef = this.chartRef.current.getContext("2d");
-      
-      new Chart(myChartRef, {
-          type: "scatter",
-          data: {
-            datasets: series
+
+  componentDidMount() {
+    console.debug('LogChart Mount', this.props.series)
+    this.myChart = new Chart(this.chartRef.current.getContext('2d'), {
+      type: "scatter",
+      data: {
+        datasets: listToDatasets(this.props.series)
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+              display: true,
+              ticks: {
+                min: moment(this.props.startDate).toDate(),
+                max: moment(this.props.endDate).toDate()
+              }
+            }
+          ]
+        },
+        tooltips: {
+          callbacks: {
+            title: (tooltipItems, data) => data.datasets[tooltipItems[0].datasetIndex].label,
+            label: (tooltipItem, data) => tooltipItem.value,
+            footer: (tooltipItems, data) => tooltipItems[0].label
           },
-          options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            legend: {
-              display: false
-            },
-            scales: {
-              xAxes: [
-                {
-                  type: 'time',
-                  display: true
-                }
-              ]
-            },
-            tooltips: {
-              callbacks: {
-                title: (tooltipItems, data) => data.datasets[tooltipItems[0].datasetIndex].label,
-                label: (tooltipItem, data) => tooltipItem.value,
-                footer: (tooltipItems, data) => tooltipItems[0].label
-              },
-              footerFontStyle: 'normal'
-            },
-          }
-      });
-    }
+          footerFontStyle: 'normal'
+        },
+      }
+    })
+  }
+
+  componentDidUpdate(prevProps) {
+    console.debug('LogChart Update', this.props.series)
+    debugger
+    this.myChart.options.scales.xAxes[0].ticks.min = moment(this.props.startDate).toDate()
+    this.myChart.options.scales.xAxes[0].ticks.max = moment(this.props.endDate).toDate()
+    this.myChart.data = { datasets: listToDatasets(this.props.series) }
+    this.myChart.update();
+    // if(prevProps != this.props && this.props && this.props.series && this.props.series.length > 0) {
+      // const myChartRef = this.chartRef.current.getContext("2d");
+    // }
   }
   render() {
+    console.debug('LogChart render')
     return (
       <canvas
         id="myChart"
--- src/routes/app.js
+++ src/routes/app.js
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'react-router-dom/Link';
+import moment from 'moment'
 import { Pane, Paneset } from '@folio/stripes/components';
-import { MultiColumnList } from '@folio/stripes-components'
+import { Datepicker, MultiColumnList, Row, Col, ButtonGroup, Button } from '@folio/stripes-components'
 import LogChart from '../components/LogChart'
 
 const App = props => {
-  const [hasError, setErrors] = useState(false);
-  const [isLoading, setLoading] = useState(true);
-  const [resp, setResp] = useState({
-    groups: {},
-    buckets: [],
-    list: []
-  });
+  const [hasError, setErrors] = useState(false)
+  const [isLoading, setLoading] = useState(true)
+  const [startDate, setStartDate] = useState(null)
+  const [state, setState] = useState({
+    rawData: [],
+    filteredData: [],
+    filters: {
+      startDate: moment().subtract(30, 'days').format('L'),
+      endDate: moment().format('L')
+    }
+  })
 
   async function fetchData() {
     const url = "http://localhost:8001/ldp/db/log"
@@ -22,25 +27,10 @@ const App = props => {
         .json()
         .then(resp => {
           setLoading(false)
-          let groups = {}, buckets = []
-          for(let i=0; i<resp.length; i++) {
-            const item = resp[i]
-            if(item['tableName'] === '') item['tableName'] = '[no table]'
-            if(!(item['tableName'] in groups)) {
-              const idx = buckets.push({
-                "label" : item['tableName'],
-                "data": []
-              })
-              groups[item['tableName']] = idx - 1
-            }
-            const idx = groups[item['tableName']]
-            buckets[idx].data.push({
-              x: new Date(item["logTime"]),
-              y: item["elapsedTime"]
-            })
-          }
-          setResp({
-            groups, buckets, list: resp
+          setState({
+            ...state,
+            rawData: resp,
+            filteredData: resp
           })
         })
         .catch(err => {
@@ -58,8 +48,57 @@ const App = props => {
     fetchData()
   }, []);
 
+  const buttonGroup = (
+    <ButtonGroup fullWidth>
+      <Button buttonStyle={'primary'} >Logs</Button>
+      <Button>Console</Button>
+      <Button>Hooks</Button>
+    </ButtonGroup>
+  )
+
+  const handleStartDateChange = e => {
+    const startDate = new Date(e.target.value)
+    let newFilteredData = state.rawData.filter(log => startDate < new Date(log.logTime))
+    if(state.filters.endDate) {
+      const endDate = new Date(state.filters.endDate)
+      newFilteredData = newFilteredData.filter(log => endDate > new Date(log.logTime))
+    }
+    const newState = {
+      ...state,
+      filteredData: newFilteredData,
+      filters: {
+        ...state.filters,
+        startDate: moment(startDate).format('L')
+      }
+    }
+    setState(newState)
+  }
+  const handleEndDateChange = e => {
+    const endDate = new Date(e.target.value)
+    let newFilteredData = state.rawData.filter(log => endDate > new Date(log.logTime))
+    if(state.filters.startDate) {
+      const startDate = new Date(state.filters.startDate)
+      newFilteredData = newFilteredData.filter(log => startDate < new Date(log.logTime))
+    }
+    const newState = {
+      ...state,
+      filteredData: newFilteredData,
+      filters: {
+        ...state.filters,
+        endDate: moment(endDate).format('L')
+      }
+    }
+    setState(newState)
+  }
+
   return (
     <Paneset>
+      <Pane defaultWidth="28%">
+        {buttonGroup}
+        {startDate}
+        <Datepicker label="Start" onChange={handleStartDateChange} value={state.filters.startDate} />
+        <Datepicker label="End" onChange={handleEndDateChange} value={state.filters.endDate} />
+      </Pane>
       <Pane defaultWidth="fill" fluidContentWidth paneTitle="ldpsystem.log">
         <div style={{
           height: '100%',
@@ -68,7 +107,14 @@ const App = props => {
           alignContent: 'stretch'
         }}>
             <div style={{ height: 200, paddingBottom: 10 }}>
-              <LogChart series={resp.buckets} />
+            { state.filteredData && state.filteredData.length > 0 ?
+              <LogChart
+                series={state.filteredData}
+                startDate={state.filters.startDate}
+                endDate={state.filters.endDate}
+              />
+              : <div/>
+            }
             </div>
             <div style={{
               position: 'relative',
@@ -80,7 +126,10 @@ const App = props => {
                 height: '100%',
                 width: '100%'
               }}>
-              { resp.list && resp.list.length > 0 ? <MultiColumnList autosize virtualize contentData={resp.list} /> : <div/>}
+              { state.filteredData && state.filteredData.length > 0 ?
+                <MultiColumnList autosize virtualize contentData={state.filteredData} />
+                : <div/>
+              }
               </div>
             </div>
         </div>
