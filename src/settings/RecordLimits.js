44e94c62975d64ad91a380d5a71d5db97c188b06
Mike Taylor <mike@indexdata.com>
2021-05-13 14:00:48 +0100

    Ongoing attempt to debug import weirdness

--- src/settings/RecordLimits.js
+++ src/settings/RecordLimits.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Pane } from '@folio/stripes/components';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
+// import { Field } from 'redux-form'; // XXX DELETE THIS LINE TO PROVOKE ERROR
+import { ConfigManager } from '@folio/stripes/smart-components';
 
-function RecordLimits({ label }) {
-  return (
-    <Pane defaultWidth="fill" fluidContentWidth paneTitle={label}>
-      <div data-test-settings-record-limits>
-        <FormattedMessage id="ui-ldp.settings.table-availability.nyi" />
-      </div>
-    </Pane>
-  );
+export default class RecordLimits extends React.Component {
+  static propTypes = {
+    stripes: PropTypes.shape({
+      connect: PropTypes.func.isRequired,
+    }).isRequired,
+    label: PropTypes.node.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.configManager = props.stripes.connect(ConfigManager);
+  }
+
+  render() {
+    return (
+      <this.configManager label={this.props.label} moduleName="LDP" configName="recordLimits">
+        blah blah blah
+      </this.configManager>
+    );
+  }
 }
-
-RecordLimits.propTypes = {
-  label: PropTypes.object.isRequired,
-};
-
-export default RecordLimits;
