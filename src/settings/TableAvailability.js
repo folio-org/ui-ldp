5ff421fc347c89428cdfd0587b52198d94dc24cc
Mike Taylor <mike@indexdata.com>
2021-05-19 18:50:42 +0100

    Initial implementation of settings page for disabling tables
    We're not there yet, though: there are three known issues:
    
    1. If coming to either this settings page or the Record Limits one
       _before_ having loaded the configuration into the LDP context, then
       what gets saved contains only the configuration elements set by the
       page in question, discarding anything previously saved by the other
       page. You can avoid this by first visiting the LDP app, which loads
       the config.
    
    2. You can add tables to the list of those that are to be disabled,
       but it seems the removing them does not work correctly.
    
    3. The configured list is not actually used by the LDP app.
    
    full LDP configuration into the LDP object, as the app does; but that
    would be clumsy. Probably better to have this done at the very top
    level as part of module startup.

--- src/settings/TableAvailability.js
+++ src/settings/TableAvailability.js
@@ -1,20 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Pane } from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
+import { Field } from 'redux-form';
+import { useStripes } from '@folio/stripes/core';
+import { ConfigManager } from '@folio/stripes/smart-components';
+import { Loading, Checkbox } from '@folio/stripes/components';
+import { useLdp } from '../LdpContext';
+import BigError from '../components/QueryBuilder/BigError';
+import getTables from '../util/getTables';
+import defaultConfig from '../util/defaultConfig';
 
-function TableAvailability({ label }) {
+function TableAvailability(props) {
+  const stripes = useStripes();
+  const ldp = useLdp();
+  const [error, setError] = useState(false);
+  const [tables, setTables] = useState(false);
+  const [ConnectedConfigManager, setConfigManager] = useState();
+
+  useEffect(() => {
+    setConfigManager(props.stripes.connect(ConfigManager));
+  }, [props.stripes]);
+
+  useEffect(() => {
+    getTables(stripes, () => null, setTables, setError);
+  }, [stripes, stripes.okapi, setTables]);
+
+  if (error) return <BigError message={error} />;
+  if (!ConnectedConfigManager || !tables) return <Loading size="xlarge" />;
+
+  const getInitialValues = (settings) => {
+    const value = settings.length === 0 ? '' : settings[0].value;
+    let config;
+
+    try {
+      config = { ...defaultConfig, ...JSON.parse(value) };
+    } catch (e) {
+      config = defaultConfig;
+    }
+
+    const res = {};
+    config.disabledTables.forEach(key => {
+      res[key] = true;
+    });
+    return res;
+  };
+
+  const beforeSave = (data) => {
+    const disabledTables = [];
+    Object.keys(data).sort().forEach(key => {
+      disabledTables.push(key);
+    });
+
+    ldp.disabledTables = disabledTables;
+    return JSON.stringify(ldp);
+  };
+
   return (
-    <Pane defaultWidth="fill" fluidContentWidth paneTitle={label}>
-      <div data-test-settings-table-availability>
-        <FormattedMessage id="ui-ldp.settings.table-availability.nyi" />
-      </div>
-    </Pane>
+    <ConnectedConfigManager
+      label={props.label}
+      moduleName="LDP"
+      configName="recordLimits"
+      getInitialValues={getInitialValues}
+      onBeforeSave={beforeSave}
+    >
+      <FormattedMessage id="ui-ldp.settings.table-availability.check-to-disable" />
+      {
+        Object.keys(tables).sort().map(key => (
+          <div key={key}>
+            <h3>
+              <FormattedMessage
+                id="ui-ldp.settings.table-availability.schema"
+                values={{
+                  name: key,
+                  code: chunks => <code>{chunks}</code>,
+                }}
+              />
+            </h3>
+            <ul style={{ listStyleType: 'none' }}>
+              {
+                tables[key]
+                  .sort((a, b) => (a.label < b.label ? -1 : a.label > b.label ? -1 : 0))
+                  .map(entry => (
+                    <li key={entry.value}>
+                      <Field
+                        component={Checkbox}
+                        type="checkbox"
+                        label={entry.label}
+                        name={`${key}-${entry.value}`}
+                      />
+                    </li>
+                  ))
+              }
+            </ul>
+          </div>
+        ))
+      }
+    </ConnectedConfigManager>
   );
 }
 
+
 TableAvailability.propTypes = {
-  label: PropTypes.object.isRequired,
+  stripes: PropTypes.shape({
+    connect: PropTypes.func.isRequired,
+  }).isRequired,
+  label: PropTypes.node.isRequired,
 };
+
 
 export default TableAvailability;
--- src/util/defaultConfig.js
+++ src/util/defaultConfig.js
@@ -1,4 +1,11 @@
-const defaultConfig = { defaultShow: 100, maxShow: 1000, maxExport: 10000 };
+const defaultConfig = {
+  // Managed in ../settings/RecordLimits.js
+  defaultShow: 100,
+  maxShow: 1000,
+  maxExport: 10000,
+
+  // Managed in ../settings/TableAvailability.js
+  disabledTables: [],
+};
 
 export default defaultConfig;
-
