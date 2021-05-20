4104069cfe948d682bf2be60e7633255008d173b
Mike Taylor <mike@indexdata.com>
2021-05-20 14:55:21 +0100

    Fixes to loadConfig
    * Tolerates being called a second time (in which case it immediate
      does `setConfigLoaded(true)` and returns)
    * Correctly copies _all_ parts of the mod-configuration response into
      the ldp object instead of just the three anointed values.

--- src/util/loadConfig.js
+++ src/util/loadConfig.js
@@ -2,22 +2,26 @@ import stripesFetch from './stripesFetch';
 import defaultConfig from './defaultConfig';
 
 const loadConfig = async (stripes, ldp, setConfigLoaded, setError) => {
-  if (!ldp.defaultShow) {
-    try {
-      const path = '/configurations/entries?query=(module==LDP and configName==recordLimits)';
-      const resp = await stripesFetch(stripes, path, { noSideLoad: true });
-      resp.json().then(json => {
-        const data = (json.configs && json.configs.length !== 0) ?
-          JSON.parse(json.configs[0].value) :
-          defaultConfig;
-        ldp.maxShow = data.maxShow;
-        ldp.maxExport = data.maxExport;
-        ldp.defaultShow = data.defaultShow;
-        setConfigLoaded(true);
-      });
-    } catch (err) {
-      setError('Could not load defaults:' + err);
-    }
+  if (ldp.defaultShow) {
+    setConfigLoaded(true);
+    return;
+  }
+
+  try {
+    const path = '/configurations/entries?query=(module==LDP and configName==recordLimits)';
+    const resp = await stripesFetch(stripes, path, { noSideLoad: true });
+    resp.json().then(json => {
+      let data;
+      if (json.configs && json.configs.length !== 0) {
+        data = JSON.parse(json.configs[0].value);
+      } else {
+        data = defaultConfig;
+      }
+      Object.assign(ldp, data); // ... showing that it CAN be used with a non-{} first argument!
+      setConfigLoaded(true);
+    });
+  } catch (err) {
+    setError('Could not load defaults:' + err);
   }
 };
 
