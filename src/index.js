cf4aee97c1eef6d97947d4b13598a49999a3aaa8
Mike Taylor <mike@indexdata.com>
2021-05-20 14:27:46 +0100

    Convert top-level index to functional component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import { NavList, NavListItem, NavListSection, Paneset, Pane } from '@folio/stripes-components';
+// import loadConfig from '../util/loadConfig';
 import { LdpContext } from './LdpContext';
 import QueryBuilderRoute from './routes/QueryBuilderRoute';
 import Logs from './routes/Logs';
@@ -16,62 +17,60 @@ const LdpConfig = {
   maxExport: null,
 };
 
-class Ldp extends React.Component {
-  static propTypes = {
-    match: PropTypes.object.isRequired,
-    actAs: PropTypes.string.isRequired,
-    stripes: PropTypes.shape({
-      okapi: PropTypes.shape({
-        url: PropTypes.string,
-        tenant: PropTypes.string,
-        token: PropTypes.string,
-      }),
-      connect: PropTypes.func
-    })
-  };
+const Ldp = (props) => {
+  const {
+    actAs,
+    stripes: {
+      okapi
+    },
+    match: {
+      path
+    }
+  } = props;
 
-  render() {
-    const {
-      actAs,
-      stripes: {
-        okapi
-      },
-      match: {
-        path
+  return (
+    <LdpContext.Provider value={LdpConfig}>
+      {actAs === 'settings' ?
+        <Settings {...props} /> :
+        <Paneset>
+          <Pane defaultWidth="15%">
+            <NavList>
+              <NavListSection activeLink={window.location.pathname}>
+                <NavListItem to={`${path}`}>Query Builder</NavListItem>
+                {/* <NavListItem to={`${path}/logs`}>Logs</NavListItem> */}
+              </NavListSection>
+            </NavList>
+          </Pane>
+
+          <Switch>
+            <Route
+              path={path}
+              exact
+              render={(props2) => <QueryBuilderRoute {...props2} okapi={okapi} />}
+            />
+            <Route
+              path={`${path}/logs`}
+              exact
+              render={(props2) => <Logs {...props2} okapi={okapi} />}
+            />
+          </Switch>
+        </Paneset>
       }
-    } = this.props;
+    </LdpContext.Provider>
+  );
+};
 
-    return (
-      <LdpContext.Provider value={LdpConfig}>
-        {actAs === 'settings' ?
-          <Settings {...this.props} /> :
-          <Paneset>
-            <Pane defaultWidth="15%">
-              <NavList>
-                <NavListSection activeLink={window.location.pathname}>
-                  <NavListItem to={`${path}`}>Query Builder</NavListItem>
-                  {/* <NavListItem to={`${path}/logs`}>Logs</NavListItem> */}
-                </NavListSection>
-              </NavList>
-            </Pane>
-
-            <Switch>
-              <Route
-                path={path}
-                exact
-                render={(props) => <QueryBuilderRoute {...props} okapi={okapi} />}
-              />
-              <Route
-                path={`${path}/logs`}
-                exact
-                render={(props) => <Logs {...props} okapi={okapi} />}
-              />
-            </Switch>
-          </Paneset>
-        }
-      </LdpContext.Provider>
-    );
-  }
-}
+Ldp.propTypes = {
+  match: PropTypes.object.isRequired,
+  actAs: PropTypes.string.isRequired,
+  stripes: PropTypes.shape({
+    okapi: PropTypes.shape({
+      url: PropTypes.string,
+      tenant: PropTypes.string,
+      token: PropTypes.string,
+    }),
+    connect: PropTypes.func
+  })
+};
 
 export default Ldp;
