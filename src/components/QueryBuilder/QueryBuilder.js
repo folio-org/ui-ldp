ff11747c691978ac6c646774352a544af289b7c5
Mike Taylor <mike@indexdata.com>
2021-05-20 15:48:30 +0100

    LDP config debug info shown in formatted varsion

--- src/components/QueryBuilder/QueryBuilder.js
+++ src/components/QueryBuilder/QueryBuilder.js
@@ -56,7 +56,7 @@ function QueryBuilder({ okapi, ldp, isLoading, initialState, tables, onSubmit, q
               <Pane id="debug" defaultWidth="fill">
                 {/* <pre>{JSON.stringify(values, 0, 2)}</pre> */}
                 {/* <Button onClick={() => { setNumTables(numTables+1) }}>Add Join Table</Button> */}
-                LDP: <code>{JSON.stringify(ldp, null, 2)}</code>
+                LDP: <pre>{JSON.stringify(ldp, null, 2)}</pre>
               </Pane>
             </form>
           </Paneset>
