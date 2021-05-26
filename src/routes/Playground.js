6957014309c8e80e1a08999370ff740b05db573a
Mike Taylor <mike@indexdata.com>
2021-05-26 17:06:27 +0100

    Slightly less broken playground

--- src/routes/Playground.js
+++ src/routes/Playground.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Paneset, Pane, PaneHeader, Button } from '@folio/stripes/components';
-import ResultsList from '../components/QueryBuilder/ResultsList';
+import { Paneset, Pane, PaneHeader, Button, MultiColumnList } from '@folio/stripes/components';
 
-const queryResponse = {
-  key: 123,
-  resp: Array(30)
-    .fill()
-    .map((_val, index) => index + 1)
-    .map(val => ({ Value: val, Squared: val * val, Cubed: val * val * val })),
-};
+const contentData = Array(30)
+  .fill()
+  .map((_val, index) => index + 1)
+  .map(val => ({ Value: val, Squared: val * val, Cubed: val * val * val }));
 
 export default function Coburn() {
   return (
@@ -26,8 +22,9 @@ export default function Coburn() {
         </div>
         <div>
           <PaneHeader paneTitle="Results" />
-          <Button fullWidth>Test</Button>
-          <ResultsList results={queryResponse} />
+          <div style={{ height: 400 }}>
+            <MultiColumnList contentData={contentData} virtualize autosize />
+          </div>
         </div>
       </div>
     </Paneset>
