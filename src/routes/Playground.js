e838a3c0fad735d153401eb8a5943d67ef04c2cf
Mike Taylor <mike@indexdata.com>
2021-05-26 18:53:06 +0100

    Playground becomes a bit more interesting

--- src/routes/Playground.js
+++ src/routes/Playground.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paneset, Pane, PaneHeader, Button, MultiColumnList } from '@folio/stripes/components';
 
-const contentData = Array(30)
-  .fill()
-  .map((_val, index) => index + 1)
-  .map(val => ({ Value: val, Squared: val * val, Cubed: val * val * val }));
-
 export default function Coburn() {
+  const [nrows, setNrows] = useState(10);
+
+  const onClick = (e) => {
+    e.preventDefault();
+    const elem = document.getElementById('nrows');
+    setNrows(parseInt(elem.value, 10));
+  };
+
+  const contentData = Array(nrows)
+    .fill()
+    .map((_val, index) => index + 1)
+    .map(val => ({ Value: val, Squared: val * val, Cubed: val * val * val }));
+
   return (
     <Paneset>
       <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
         <div style={{ height: 'auto' }}>
           <Paneset nested static>
             <Pane defaultWidth="50%" paneTitle="Query">
-              Query content
+              <form onSubmit={onClick}>
+                Number of rows to display:
+                {' '}
+                <input id="nrows" defaultValue={nrows} />
+                <br />
+                <Button type="submit" onClick={onClick}>
+                  Go!
+                </Button>
+              </form>
             </Pane>
             <Pane defaultWidth="fill" paneTitle="Debug info">
-              Debug content
+              nrows = {typeof nrows} {nrows}
+              <pre>
+                {JSON.stringify(contentData[contentData.length-1], null, 2)}
+              </pre>
             </Pane>
           </Paneset>
         </div>
