1b5b01cd8a8475edc4008609f679a4a8d103d14e
Mike Taylor <mike@indexdata.com>
2021-05-26 16:36:05 +0100

    Formatting

--- src/routes/Playground.js
+++ src/routes/Playground.js
@@ -6,38 +6,30 @@ const queryResponse = {
   key: 123,
   resp: Array(30)
     .fill()
-    .map((_val, index) => index+1)
+    .map((_val, index) => index + 1)
     .map(val => ({ Value: val, Squared: val * val, Cubed: val * val * val })),
 };
 
 export default function Coburn() {
   return (
     <Paneset>
-      <>
-        <>
-          <>
-            <>
-              <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-                <div style={{ height: 'auto' }}>
-                  <Paneset nested static>
-                    <Pane defaultWidth="50%" paneTitle="Query">
-                      Query content
-                    </Pane>
-                    <Pane defaultWidth="fill" paneTitle="Debug info">
-                      Debug content
-                    </Pane>
-                  </Paneset>
-                </div>
-                <div>
-                  <PaneHeader paneTitle="Results" />
-                  <Button fullWidth>Test</Button>
-                  <ResultsList results={queryResponse} />
-                </div>
-              </div>
-            </>
-          </>
-        </>
-      </>
+      <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        <div style={{ height: 'auto' }}>
+          <Paneset nested static>
+            <Pane defaultWidth="50%" paneTitle="Query">
+              Query content
+            </Pane>
+            <Pane defaultWidth="fill" paneTitle="Debug info">
+              Debug content
+            </Pane>
+          </Paneset>
+        </div>
+        <div>
+          <PaneHeader paneTitle="Results" />
+          <Button fullWidth>Test</Button>
+          <ResultsList results={queryResponse} />
+        </div>
+      </div>
     </Paneset>
   );
 }
