8e6d59e66176f5bd408ae52300194140e152fd4f
Mike Taylor <mike@miketaylor.org.uk>
2021-09-30 15:45:59 +0100

    Tweak dev config
    * Do not try to run against local mod-ldp
    * Turn off `showDevInfo`

--- stripes.config.js
+++ stripes.config.js
@@ -2,8 +2,8 @@ module.exports = {
   okapi: { url: 'https://folio-snapshot-okapi.dev.folio.org', tenant: 'diku' },
   config: {
     showHomeLink: true,
-    modLdpUrl: 'http://localhost:8080/http://localhost:8001',
-    showDevInfo: true,
+    // modLdpUrl: 'http://localhost:8080/http://localhost:8001',
+    // showDevInfo: true,
   },
   modules: {
     '@folio/ldp': {},
