d67159c2a2ee23ce2373b920ccd548ba39a779e8
Mike Taylor <mike@miketaylor.org.uk>
2021-11-01 19:08:52 +0000

    Actual saving of DB config

--- src/settings/DatabaseConfig.js
+++ src/settings/DatabaseConfig.js
@@ -1,23 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { useStripes } from '@folio/stripes/core';
-import { Pane, LoadingPane } from '@folio/stripes/components';
+import { useStripes, CalloutContext } from '@folio/stripes/core';
+import { LoadingPane, Pane, Row, Col, TextField, Button } from '@folio/stripes/components';
 import BigError from '../components/BigError';
 import stripesFetch from '../util/stripesFetch';
 
 
 function DatabaseConfig(props) {
-  const [config, setConfig] = useState();
+  const [loadedConfig, setLoadedConfig] = useState();
+  const [url, setUrl] = useState();
+  const [user, setUser] = useState();
+  const [pass, setPass] = useState();
   const [error, setError] = useState();
+  const [submitting, setSubmitting] = useState(false);
   const stripes = useStripes();
+  const callout = useContext(CalloutContext);
 
   useEffect(() => {
     async function fetchData() {
       const res = await stripesFetch(stripes, '/ldp/config/dbinfo');
       if (res.ok) {
         const json = await res.json();
-        setConfig(JSON.parse(json.value));
+        const data = JSON.parse(json.value);
+        setLoadedConfig(data);
+        setUrl(data.url);
+        setUser(data.user);
+        setPass(data.pass);
       } else {
         const content = await res.text();
         setError(`${res.statusText}: ${content}`);
@@ -27,25 +36,71 @@ function DatabaseConfig(props) {
   }, [stripes]);
 
   if (error) return <BigError message={error} />;
-  if (!config) return <LoadingPane />;
+  if (!loadedConfig) return <LoadingPane />;
+
+  const saveData = async () => {
+    setSubmitting(true);
+    const res = await stripesFetch(stripes, '/ldp/config/dbinfo', {
+      method: 'PUT',
+      body: JSON.stringify({
+        key: 'dbinfo',
+        tenant: stripes.okapi.tenant,
+        value: JSON.stringify({
+          url, user, pass,
+        }),
+      }),
+    });
+
+    setSubmitting(false);
+    if (res.ok) {
+      callout.sendCallout({
+        message: <FormattedMessage id="ui-ldp.settings.database-configuration.update.ok" />
+      });
+    } else {
+      const content = await res.text();
+      setError(`${res.statusText}: ${content}`);
+    }
+  };
+
+  const disabled = (submitting ||
+                    (url === loadedConfig.url &&
+                     user === loadedConfig.user &&
+                     pass === loadedConfig.pass));
 
   return (
     <Pane paneTitle={props.label} defaultWidth="fill">
-      <p>
-        <FormattedMessage id="ui-ldp.settings.database-configuration.url" />:
-        &nbsp;
-        <b>{config.url}</b>
-      </p>
-      <p>
-        <FormattedMessage id="ui-ldp.settings.database-configuration.user" />:
-        &nbsp;
-        <b>{config.user}</b>
-      </p>
-      <p>
-        <FormattedMessage id="ui-ldp.settings.database-configuration.pass" />:
-        &nbsp;
-        <b>{config.pass}</b>
-      </p>
+      <Row>
+        <Col xs={12}>
+          <TextField
+            label={<FormattedMessage id="ui-ldp.settings.database-configuration.url" />}
+            value={url}
+            onChange={e => setUrl(e.target.value)}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6}>
+          <TextField
+            label={<FormattedMessage id="ui-ldp.settings.database-configuration.user" />}
+            value={user}
+            onChange={e => setUser(e.target.value)}
+          />
+        </Col>
+        <Col xs={6}>
+          <TextField
+            label={<FormattedMessage id="ui-ldp.settings.database-configuration.pass" />}
+            value={pass}
+            onChange={e => setPass(e.target.value)}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <Button buttonStyle="primary" onClick={saveData} disabled={disabled}>
+            <FormattedMessage id="ui-ldp.button.submit" />
+          </Button>
+        </Col>
+      </Row>
     </Pane>
   );
 }
