03adee738bba14b2f2089634bcc65617338549c9
Mike Taylor <mike@miketaylor.org.uk>
2021-11-05 10:18:55 +0000

    Add comments with GitHub API notes

--- src/util/gitHubFetch.js
+++ src/util/gitHubFetch.js
@@ -1,5 +1,15 @@
 import fetch from 'cross-fetch';
 
+// GitHub API notes
+//
+// User-Agent header is required, can be name of app, e.g. `User-Agent: Awesome-Octocat-App`
+// Send header `Accept: application/vnd.github.v3+json`
+// Authentication method 1 (HTTP Basic): `curl -u "username:password" https://api.github.com`
+// Authentication method 2 preferred (OAuth2 token): `curl -H "Authorization: token OAUTH-TOKEN" https://api.github.com`
+// See https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api#using-personal-access-tokens
+// Actually fetching files is from a specific commit or branch:
+// see https://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api
+  
 async function gitHubFetch(config, path, options) {
   return fetch(`https://api.github.com/${path}`, {
     ...options,
