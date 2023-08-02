bc9936841a2a84a678a9cfb790e098f1510b74ea
Mike Taylor <mike@indexdata.com>
2023-08-02 19:45:29 +0100

    Show MCL of templated queries, with select

--- src/components/TemplatedQueries/TemplatedQueries.js
+++ src/components/TemplatedQueries/TemplatedQueries.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Paneset, Pane, NoValue } from '@folio/stripes/components';
+import { FormattedMessage } from 'react-intl';
+import { Paneset, Pane, MultiColumnList, NoValue } from '@folio/stripes/components';
 
 function TemplatedQueries({ queries }) {
+  const [query, setQuery] = useState();
+
   return (
     <Paneset>
-      <Pane defaultWidth="50%" paneTitle="Select templated query">
-        <pre>{JSON.stringify(queries, null, 2)}</pre>
+      <Pane defaultWidth="fill" paneTitle="Select templated query">
+        <MultiColumnList
+          contentData={queries}
+          visibleColumns={['displayName', 'filename', 'repo']}
+          columnMapping={{
+            displayName: <FormattedMessage id="ui-ldp.templated-queries.column.displayName" />,
+            filename: <FormattedMessage id="ui-ldp.templated-queries.column.filename" />,
+            repo: <FormattedMessage id="ui-ldp.templated-queries.column.repo" />,
+          }}
+          columnWidths={{
+          }}
+          formatter={{
+            displayName: r => r.json?.displayName,
+            repo: r => `${r.config.user}/${r.config.repo}`,
+          }}
+          onRowClick={(_, q) => setQuery(q)}
+        />
       </Pane>
-      <Pane defaultWidth="fill" paneTitle="Fill in query parameters">
-        <NoValue />
-      </Pane>
+      {query &&
+        <Pane defaultWidth="50%" paneTitle="Fill in query parameters">
+          <NoValue />
+          <pre>{JSON.stringify(query, null, 2)}</pre>
+        </Pane>
+      }
     </Paneset>
   );
 }
@@ -18,7 +39,15 @@ function TemplatedQueries({ queries }) {
 TemplatedQueries.propTypes = {
   queries: PropTypes.arrayOf(
     PropTypes.shape({
-      // name: PropTypes.string.isRequired,
+      filename: PropTypes.string.isRequired,
+      config: PropTypes.shape({
+        user: PropTypes.string.isRequired,
+        repo: PropTypes.string.isRequired,
+      }).isRequired,
+      text: PropTypes.string.isRequired,
+      json: PropTypes.shape({
+        displayName: PropTypes.string,
+      }),
     }).isRequired,
   ),
 };
