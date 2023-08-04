9f03242ecd627174a97275606eebb8e627a13f83
Mike Taylor <mike@indexdata.com>
2023-08-04 11:06:08 +0100

    Error message for TQs with no JSON metadata

--- src/components/TemplatedQuery/TemplatedQuery.js
+++ src/components/TemplatedQuery/TemplatedQuery.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Pane } from '@folio/stripes/components';
+import { FormattedMessage } from 'react-intl';
+import { Pane, Accordion } from '@folio/stripes/components';
+import css from './TemplatedQuery.css';
 
 function TemplatedQuery({ query }) {
   const title = query.json?.displayName || query.name;
   return (
     <Pane defaultWidth="fill" paneTitle={title}>
-      <pre>{JSON.stringify(query, null, 2)}</pre>
+      {!query.json ? (
+        <div className={css.noJsonError}>
+          <FormattedMessage id="ui-ldp.templated-queries.no-json" />
+        </div>
+      ) : (
+        <div>XXX actual stuff</div>
+      )}
+      <Accordion closedByDefault label={<FormattedMessage id="ui-ldp.devinfo" />}>
+        <pre>{JSON.stringify(query, null, 2)}</pre>
+      </Accordion>
     </Pane>
   );
 }
