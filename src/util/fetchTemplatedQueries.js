8fa2f8d03100e1b016dd08cdfb5e9b20e0f18149
Mike Taylor <mike@indexdata.com>
2023-10-18 16:18:03 +0100

    Available reports are now sorted correctly
    Fixes UILDP-122.

--- src/util/fetchTemplatedQueries.js
+++ src/util/fetchTemplatedQueries.js
@@ -105,6 +105,7 @@ async function fetchTemplatedQueries(gitRepos, setQueries) {
   }));
 
   const merged = mergeSQLandJSON(data);
+  merged.sort((a, b) => a.json?.displayName.localeCompare(b.json?.displayName));
   setQueries(merged);
 }
 
