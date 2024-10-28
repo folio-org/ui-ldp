6feaf6e5c9989d6694afd6b0d1da579fc02d7765
Mike Taylor <mike@indexdata.com>
2024-10-28 16:01:21 +0000

    First draft of editing for list of charts in dashboard

--- src/components/DashboardForm/DashboardForm.js
+++ src/components/DashboardForm/DashboardForm.js
@@ -2,21 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Form, Field } from 'react-final-form';
-import { Pane, TextField, TextArea, Button } from '@folio/stripes/components';
+import arrayMutators from 'final-form-arrays';
+import { FieldArray } from 'react-final-form-arrays';
+import { Pane, TextField, TextArea, Select, Button } from '@folio/stripes/components';
 
 
 function DashboardForm({ data, onSubmit }) {
   const header = <FormattedMessage id="ui-ldp.dashboard.editHeading" values={{ name: data.dashboard.value.name }} />;
 
   const initialValues = { ...data.dashboard.value };
+  const dataOptions = data.allCharts.map(x => ({ value: x.id, label: x.value.name }));
 
   return (
     <Pane defaultWidth="fill" paneTitle={header}>
-      <Form initialValues={initialValues} onSubmit={onSubmit}>
+      <Form initialValues={initialValues} onSubmit={onSubmit} mutators={{ ...arrayMutators }}>
         {({ handleSubmit }) => (
           <form onSubmit={handleSubmit}>
             <Field name="name" label={<FormattedMessage id="ui-ldp.dashboard.name" />} required component={TextField} />
             <Field name="description" label={<FormattedMessage id="ui-ldp.dashboard.description" />} component={TextArea} />
+
+            <FieldArray name="charts">
+              {({ fields }) => (
+                <div>
+                  {fields.map((name, index) => (
+                    <div key={index}>
+                      <Field name={name} label="ID" component={Select} dataOptions={dataOptions} />
+                      <button type="button" onClick={() => fields.remove(index)}>Remove</button>
+                    </div>
+                  ))}
+                  <button type="button" onClick={() => fields.push('')}>Add</button>
+                </div>
+              )}
+            </FieldArray>
+
             <Button type="submit">
               <FormattedMessage id="ui-ldp.button.submit" />
             </Button>
