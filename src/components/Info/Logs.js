cb2bd21651e4eee6a7f87de87a1db02fc9bca20d
Mike Taylor <mike@indexdata.com>
2025-02-04 18:09:27 +0000

    Nice sortable list of log entries

--- src/components/Info/Logs.js
+++ src/components/Info/Logs.js
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { MultiColumnList } from '@folio/stripes/components';
 import Tabs from '../../Tabs';
 
 
+// PRIVATE to colorize
+const _severityColors = {
+  INFO: '#808080',
+  WARNING: '#e0a000',
+  ERROR: '#e00000',
+  FATAL: '#000000',
+};
+
+
+function colorize(severity) {
+  const color = _severityColors[severity];
+  return <span style={{ color }}>{severity}</span>;
+}
+
+
 function Logs({ data }) {
+  const [sortedColumn, setSortedColumn] = useState('log_time');
+  const [sortDirection, setSortDirection] = useState('descending');
+
+  const sortedLogs = (data.logs || []).sort((a, b) => {
+    const av = a[sortedColumn];
+    const bv = b[sortedColumn];
+
+    if (av < bv) {
+      return (sortDirection == 'ascending' ? -1 : 1);
+    } else if (av > bv) {
+      return (sortDirection == 'ascending' ? 1 : -1);
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <Tabs>
-      <pre>{JSON.stringify(data.logs, null, 2)}</pre>
+      <MultiColumnList
+        contentData={sortedLogs}
+        visibleColumns={['log_time', 'error_severity', 'message']}
+        columnMapping={{
+          log_time: <FormattedMessage id="ui-ldp.dbinfo.logs.log_time" />,
+          error_severity: <FormattedMessage id="ui-ldp.dbinfo.logs.error_severity" />,
+          message: <FormattedMessage id="ui-ldp.dbinfo.logs.message" />,
+        }}
+        columnWidths={{
+          log_time: 200,
+          error_severity: 100,
+          message: 600,
+        }}
+        formatter={{
+          log_time: r => r.log_time, // XXX localize
+          error_severity: r => colorize(r.error_severity)
+        }}
+        sortedColumn={sortedColumn}
+        sortDirection={sortDirection}
+        onHeaderClick={(e, { name }) => {
+          console.log('clicked on', name);
+          if (name === sortedColumn) {
+            setSortDirection(sortDirection === 'ascending' ? 'descending' : 'ascending');
+          } else {
+            setSortedColumn(name);
+            setSortDirection('ascending');
+          }
+        }}
+      />
     </Tabs>
   );
 }
