747acad794de12f0766bff7d6a5842e13653523d
Mike Taylor <mike@miketaylor.org.uk>
2025-05-15 19:38:41 +0100

    [UILDP-141] configurable controlled vocabs for report forms
    When defining a form-filling field in the JSON metadata of a report, it is now possible to specify that the field draws its candidate fields from the result of a FOLIO request — for example to offer the user a list of the locations in the FOLIO system.
    
    See documentation in doc/reports.md#writing-json-metadata-for-your-report

--- src/components/TemplatedQuery/TemplatedQueryForm.js
+++ src/components/TemplatedQuery/TemplatedQueryForm.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Form, Field } from 'react-final-form';
-import { TextField, Datepicker, Select, AutoSuggest, Button, Accordion } from '@folio/stripes/components';
+import { useOkapiKy } from '@folio/stripes/core';
+import { TextField, Datepicker, Select, AutoSuggest, Loading, Button, Accordion } from '@folio/stripes/components';
 import baseName from '../../util/baseName';
 import { createReportRepo } from '../../util/repoTypes';
 import ResultsList from '../QueryBuilder/ResultsList';
+import fetchOptions from '../../util/fetchOptions';
 
 
 function type2component(param) {
@@ -14,6 +16,8 @@ function type2component(param) {
   } else if (param.type !== 'text') {
     // We could throw an "unsupported type" error, but it's probably friendlier to default to text
     return [TextField, 'DEFAULT'];
+  } else if (param['controlled.fetchOptions']) {
+    return [Select, 'Select'];
   } else if (param['controlled.options'] && !param['controlled.allowOtherValues']) {
     return [Select, 'Select'];
   } else if (param['controlled.options'] && param['controlled.allowOtherValues']) {
@@ -24,13 +28,19 @@ function type2component(param) {
 }
 
 
-function type2options(param) {
+async function type2options(okapiKy, param) {
+  const foVal = param['controlled.fetchOptions'];
+  if (foVal) {
+    return fetchOptions(okapiKy, foVal);
+  }
+
   const rawData = param['controlled.options'];
   if (!rawData || param['controlled.allowOtherValues']) return undefined;
   return rawData.map(x => ({ value: x, label: x }));
 }
 
 
+// For some reason, <AutoSuggest> wants an `item` prop instead of `options`
 function type2items(param) {
   const rawData = param['controlled.options'];
   if (!rawData || !param['controlled.allowOtherValues']) return undefined;
@@ -38,9 +48,9 @@ function type2items(param) {
 }
 
 
-function parameterizedField(param) {
+async function parameterizedField(okapiKy, param) {
   const [component, _cname] = type2component(param);
-  const dataOptions = type2options(param);
+  const dataOptions = await type2options(okapiKy, param);
   const items = type2items(param);
 
   const extraParams = {};
@@ -64,6 +74,21 @@ function TemplatedQueryForm({ query, onSubmit, data }) {
   const { json } = query;
   const reportRepo = createReportRepo(query.config);
   const urlBase = reportRepo.urlBase(query.filename);
+  const okapiKy = useOkapiKy();
+  const [fields, setFields] = useState(null);
+
+  // We have to omit okapiKy from the hook dependency below, otherwise
+  // this hook gets called on each render, causing an infinite loop.
+  useEffect(() => {
+    const loadFields = async () => {
+      const activeParams = json.parameters.filter(param => !param.disabled);
+      const promises = activeParams.map(param => parameterizedField(okapiKy, param));
+      const renderedFields = await Promise.all(promises);
+      setFields(renderedFields);
+    };
+
+    loadFields();
+  }, [json]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const initialValues = {};
   json.parameters.forEach(param => {
@@ -95,10 +120,7 @@ function TemplatedQueryForm({ query, onSubmit, data }) {
         {({ handleSubmit }) => (
           data ? <ResultsList results={data} /> :
           <form onSubmit={handleSubmit}>
-            {json.parameters
-              .filter(param => !param.disabled)
-              .map(param => parameterizedField(param))
-            }
+            {fields || <Loading />}
             <Button type="submit">
               <FormattedMessage id="ui-ldp.button.submit" />
             </Button>
