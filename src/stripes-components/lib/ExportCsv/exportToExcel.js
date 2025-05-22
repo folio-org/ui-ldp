4dfbb7e0340bb13acd799ee0a932e58eb8b1f835
Mike Taylor <mike@indexdata.com>
2025-05-22 16:39:15 +0100

    Honour the `header` option

--- src/stripes-components/lib/ExportCsv/exportToExcel.js
+++ src/stripes-components/lib/ExportCsv/exportToExcel.js
@@ -4,9 +4,9 @@ import exportToSpreadsheet from './exportToSpreadsheet';
 const XLSX_MIMETYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
 export default function exportToExcel(objectArray, opts) {
-  return exportToSpreadsheet(objectArray, opts, 'xlsx', 'Excel', (_fields, _header) => {
-    // XXX `_fields` and `_header` are not yet used
-    const worksheet = XLSX.utils.json_to_sheet(objectArray);
+  return exportToSpreadsheet(objectArray, opts, 'xlsx', 'Excel', (_fields, header) => {
+    // XXX `_fields` is not yet used
+    const worksheet = XLSX.utils.json_to_sheet(objectArray, { skipHeader: !header });
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'FOLIO Reporting export');
     const rawData = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx', compression: true });
