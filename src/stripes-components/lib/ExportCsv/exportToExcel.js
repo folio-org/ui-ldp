143a60289ebaa0682a20f216e5eb91acf333a57b
Mike Taylor <mike@indexdata.com>
2025-05-22 17:46:06 +0100

    Support `fields` parameter

--- src/stripes-components/lib/ExportCsv/exportToExcel.js
+++ src/stripes-components/lib/ExportCsv/exportToExcel.js
@@ -4,9 +4,11 @@ import exportToSpreadsheet from './exportToSpreadsheet';
 const XLSX_MIMETYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
 export default function exportToExcel(objectArray, opts) {
-  return exportToSpreadsheet(objectArray, opts, 'xlsx', 'Excel', (_fields, header) => {
-    // XXX `_fields` is not yet supported
-    const worksheet = XLSX.utils.json_to_sheet(objectArray, { skipHeader: !header });
+  return exportToSpreadsheet(objectArray, opts, 'xlsx', 'Excel', (fields, header) => {
+    console.log('fields =', fields);
+    // The mapping of our option names to those of XLSX is rather confusing!
+    // See https://www.npmjs.com/package/xlsx#array-of-objects-input on inclusion of unspecified fields
+    const worksheet = XLSX.utils.json_to_sheet(objectArray, { header: fields, skipHeader: !header });
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'FOLIO Reporting export');
     const rawData = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx', compression: true });
