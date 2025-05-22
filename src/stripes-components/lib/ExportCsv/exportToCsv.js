f65594ba764608e995a0c550d684c701ba26c78c
Mike Taylor <mike@indexdata.com>
2025-05-22 16:06:33 +0100

    Simplify

--- src/stripes-components/lib/ExportCsv/exportToCsv.js
+++ src/stripes-components/lib/ExportCsv/exportToCsv.js
@@ -4,12 +4,10 @@ import exportToSpreadsheet from './exportToSpreadsheet';
 const CSV_MIMETYPE = 'text/csv;charset=utf-8;';
 
 export default function exportToCsv(objectArray, opts) {
-  function dataToCsv(fields, header) {
+  return exportToSpreadsheet(objectArray, opts, 'csv', 'CSV', (fields, header) => {
     const parser = new Parser({ fields, flatten: true, header });
     const rawData = parser.parse(objectArray);
     const blob = new Blob(['\ufeff', rawData], { type: CSV_MIMETYPE });
     return blob;
-  }
-
-  return exportToSpreadsheet(objectArray, opts, 'csv', 'CSV', dataToCsv);
+  });
 }
--- src/stripes-components/lib/ExportCsv/exportToExcel.js
+++ src/stripes-components/lib/ExportCsv/exportToExcel.js
@@ -4,16 +4,12 @@ import exportToSpreadsheet from './exportToSpreadsheet';
 const XLSX_MIMETYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
 export default function exportToExcel(objectArray, opts) {
-  function dataToExcel(_fields, _header) {
-    // XXX `_fields` is not yet used
-    // XXX `_header` is not yet used
+  return exportToSpreadsheet(objectArray, opts, 'xlsx', 'Excel', (_fields, _header) => {
+    // XXX `_fields` and `_header` are not yet used
     const worksheet = XLSX.utils.json_to_sheet(objectArray);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'FOLIO Reporting export');
     const rawData = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx', compression: true });
-    const blob = new Blob([rawData], { type: XLSX_MIMETYPE });
-    return blob;
-  }
-
-  return exportToSpreadsheet(objectArray, opts, 'xlsx', 'Excel', dataToExcel);
+    return new Blob([rawData], { type: XLSX_MIMETYPE });
+  });
 }
