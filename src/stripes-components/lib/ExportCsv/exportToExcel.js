17009fd7c9ebe8bdfea71b9aa50fc7d1f3e8843f
Mike Taylor <mike@miketaylor.org.uk>
2025-05-23 09:30:18 +0100

    [UILDP-175] Switch to `@e965/xslt` for security patches (#324)
    Switch from the `xslt` dependency, which while maintained is no longer
    published to NPM, to the mirror version `@e965/xslt`. This fixes two
    known security issues.

--- src/stripes-components/lib/ExportCsv/exportToExcel.js
+++ src/stripes-components/lib/ExportCsv/exportToExcel.js
@@ -1,11 +1,10 @@
-import * as XLSX from 'xlsx';
+import * as XLSX from '@e965/xlsx';
 import exportToSpreadsheet from './exportToSpreadsheet';
 
 const XLSX_MIMETYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
 export default function exportToExcel(objectArray, opts) {
   return exportToSpreadsheet(objectArray, opts, 'xlsx', 'Excel', (fields, header) => {
-    console.log('fields =', fields);
     // The mapping of our option names to those of XLSX is rather confusing!
     // See https://www.npmjs.com/package/xlsx#array-of-objects-input on inclusion of unspecified fields
     const worksheet = XLSX.utils.json_to_sheet(objectArray, { header: fields, skipHeader: !header });
