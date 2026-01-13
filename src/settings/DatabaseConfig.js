a66017f0f7651b719c9b3cfebf41060f2a080a38
Mike Taylor <mike@indexdata.com>
2026-01-13 13:56:03 +0000

    [UILDP-192] DB Config settings page uses `<ConfigManager>`
    Database Configuration settings page uses `<ConfigManager>` like
    Record Limits and Table Availability, so it can now create a new
    settings object as well as modify an existing one.

--- src/settings/DatabaseConfig.js
+++ src/settings/DatabaseConfig.js
@@ -1,21 +1,92 @@
-import React from 'react';
-import { useIntl } from 'react-intl';
-import LdpConfig from './components/LdpConfig';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useIntl, FormattedMessage } from 'react-intl';
+import { Field } from 'react-final-form';
+import { ConfigManager } from '@folio/stripes/smart-components';
+import { Col, Row, TextField } from '@folio/stripes/components';
+
 
 function DatabaseConfig(props) {
   const intl = useIntl();
+  const [ConnectedConfigManager, setConfigManager] = useState();
+
+  useEffect(() => {
+    setConfigManager(props.stripes.connect(ConfigManager));
+  }, [props.stripes]);
+
+  if (!ConnectedConfigManager) return null;
+
+  const getInitialValues = (settings) => {
+    const value = settings.length === 0 ? '{}' : settings[0].value;
+    const config = JSON.parse(value);
+    return { ...config, pass: config.pass ? '********' : '' };
+  };
+
+  const beforeSave = (data) => {
+    return JSON.stringify(data);
+  };
 
   return (
-    <LdpConfig
-      {...props}
-      configKey="dbinfo"
-      fields={[
-        { name: 'url', xs: 12 },
-        { name: 'user', xs: 6 },
-        { name: 'pass', xs: 6, placeholder: intl.formatMessage({ id: 'ui-ldp.placeholder.hidden' }) },
-      ]}
-    />
+    <ConnectedConfigManager
+      formType="final-form"
+      label={props.label}
+      scope="ui-ldp.admin"
+      configName="dbinfo"
+      getInitialValues={getInitialValues}
+      onBeforeSave={beforeSave}
+    >
+      <Row>
+        <Col xs={12} id="url">
+          <FormattedMessage id="ui-ldp.settings.dbinfo.url">
+            {label => (
+              <Field
+                id="url"
+                name="url"
+                label={label}
+                component={TextField}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6} id="user">
+          <FormattedMessage id="ui-ldp.settings.dbinfo.user">
+            {label => (
+              <Field
+                id="user"
+                name="user"
+                label={label}
+                component={TextField}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+        <Col xs={6} id="pass">
+          <FormattedMessage id="ui-ldp.settings.dbinfo.pass">
+            {label => (
+              <Field
+                id="pass"
+                name="pass"
+                label={label}
+                component={TextField}
+                placeholder={intl.formatMessage({ id: 'ui-ldp.placeholder.hidden' })}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+      </Row>
+    </ConnectedConfigManager>
   );
 }
+
+
+DatabaseConfig.propTypes = {
+  stripes: PropTypes.shape({
+    connect: PropTypes.func.isRequired,
+  }).isRequired,
+  label: PropTypes.node.isRequired,
+};
+
 
 export default DatabaseConfig;
