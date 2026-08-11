622f8419c50d046ac4539a258466bad2a6f73502
Mike Taylor <mike@indexdata.com>
2026-08-11 10:05:51 +0200

    More elegant templatedQueryName function avoids redundant dir

--- src/util/templatedQueryName.js
+++ src/util/templatedQueryName.js
@@ -1,5 +1,10 @@
 import baseName from './baseName';
 
+// The filename is already relative to the root of the repository --
+// both repository types populate it from the path of the tree entry
+// -- so it includes the configured directory {q.config.dir}, and we
+// need not repeat it.
+//
 export default function templatedQueryName(q) {
-  return `${q.config.user}/${q.config.repo}/${q.config.branch}/${q.config.dir}/${baseName(q.filename)}`;
+  return `${q.config.user}/${q.config.repo}/${q.config.branch}/${baseName(q.filename)}`;
 }
